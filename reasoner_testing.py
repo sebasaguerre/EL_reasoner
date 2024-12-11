e14eab7e5620d7ffd2231c9257b939814e9a2367
chrisomlaute@gmail.com <chrisomlaute@gmail.com>
2024-12-11 11:24:38 +0100

    testing

---
+++ reasoner_testing.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+from py4j.java_gateway import JavaGateway
+
+# connect to the java gateway of dl4python
+gateway = JavaGateway()
+
+# get a parser from OWL files to DL ontologies
+parser = gateway.getOWLParser()
+
+# get a formatter to print in nice DL format
+formatter = gateway.getSimpleDLFormatter()
+
+print("Loading the ontology...")
+
+# load an ontology from a file
+ontology = parser.parseFile("cto.clinical-trials-ontology.1.owl.xml")
+
+print("Loaded the ontology!")
+
+# IMPORTANT: the algorithm from the lecture assumes conjunctions to always be over two concepts
+# Ontologies in OWL can however have conjunctions over an arbitrary number of concpets.
+# The following command changes all conjunctions so that they have at most two conjuncts
+print("Converting to binary conjunctions")
+gateway.convertToBinaryConjunctions(ontology)
